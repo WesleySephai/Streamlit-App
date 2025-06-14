7008c3762829a1eae3fa396a33aac7c165009fac
WesleySephai <124874511+WesleySephai@users.noreply.github.com>
2025-06-14 13:25:24 +0200

    Create model.py
    Expense Tracker with Budget Insights |HTML, Streamlit, CSS, Flask, PostgreSQL

---
+++ expense_traker/model.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    expenses = db.relationship('Expense', backref='user', lazy=True)
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category = db.Column(db.String(100), nullable=False)
+    amount = db.Column(db.Float, nullable=False)
+    date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
